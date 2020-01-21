1a6b76f5817260ce93000dd695e1da79f348f790
Jeff <rustyangel@gmail.com>
2020-01-21 09:57:45 -0700

    Use new typescript-eslint parser/plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
-    parser: 'typescript-eslint-parser',
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
     extends: ['@zazen/eslint-config'],
-    plugins: ['typescript'],
     rules: {},
 }
