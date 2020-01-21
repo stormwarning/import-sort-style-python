48596855ac2a54a62b1cf84dc61507aa277aed3a
Jeff <rustyangel@gmail.com>
2020-01-21 12:57:21 -0700

    Update eslint setup
    Also removes `microbundle` since it kept leaving its cache dirs behind.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,5 +2,9 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     extends: ['@zazen/eslint-config'],
-    rules: {},
+    rules: {
+        // eslint doesn't think my vars are used ¯\_(ツ)_/¯
+        'no-unused-vars': 'off',
+        'prefer-const': 'off',
+    },
 }
